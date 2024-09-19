ce9ca130f8c37143ae1c4d365354c7fb6737c146
sashammiriti <sasha.miriti@strathmore.edu>
2024-09-20 00:12:02 +0300

    Bootstrap demo,sign-up

--- layouts/layout.php
+++ layouts/layout.php
@@ -2,30 +2,32 @@
 class layout{
     public function heading(){
         ?>
-        <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="css/style.css">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/custom.css">
+  
 </head>
 <body>
-    <?php
-   /* require_once "load.php";
-    print $Obj->user_age("Marlyn", 2004); */
-?>
-
-<?php
+        <?php
     }
     public function footer(){
         ?>
-<div class="footer">
-    Copyright $copy; ICS <?php print date("Y"); ?>
-</div>
+ 
+<footer class="pt-3 mt-4 text-body-secondary border-top">
+Copyright &copy; ICS <?php print date("Y"); ?>
+      </footer>
+    </div>
+  </main>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
-<?php 
+        <?php
     }
 }
-?>
--- load.php
+++ load.php
@@ -1,69 +1,45 @@
 <?php
+
+// Class Auto Load 
+
 function classAutoLoad($classname){
 
+    $directories = ["contents", "layouts", "menus", "forms"];
 
-$directories = ["contents", "layouts", "menus"];
-
-foreach($directories AS $dir)
-{
-    $filename = dirname(__FILE__) . DIRECTORY_SEPARATOR . $dir .  DIRECTORY_SEPARATOR . $classname . ".php";
-    if(file_exists($filename) AND is_readable($filename)){
-        require_once $filename;
+    foreach($directories AS $dir){
+        $filename = dirname(__FILE__) . DIRECTORY_SEPARATOR . $dir . DIRECTORY_SEPARATOR . $classname . ".php";
+        if(file_exists($filename) AND is_readable($filename)){
+            require_once $filename;
+        }
     }
 }
-}
+
 spl_autoload_register('classAutoLoad');
-//Create instance of all classes
-$ObjLayouts = new layout();
-$ObjMenus = new menus();
-$ObjHeadings = new headings();
-$ObjCont = new contents();
+
+// Create instances of all classes
+    $ObjLayouts = new layout();
+    $ObjMenus = new menus();
+    $ObjHeadings = new headings();
+    $ObjCont = new contents();
+    $ObjForm = new user_forms();
 
 require "includes/constants.php";
 require "includes/dbConnection.php";
 
-$conn = new dbConnection(DBTYPE, HOSTNAME, DBPORT, HOSTUSER, HOSTPASS,DBNAME);
-
-/*<!-- print date("1");
-if(date("1")== "Friday"){
-    print "Yes";
-}else{
-    print "No";
-}
-
-print "<br>";
-switch (date("1")){
-    case 'Friday': print "Yes";
-    break;
-    case 'Monday': print "No";
-    break;
-} -->*/
+$conn = new dbConnection(DBTYPE, HOSTNAME, DBPORT, HOSTUSER, HOSTPASS, DBNAME);
 
 
-
-/*$arr = ["black", "white", "green", "red"];
-
-foreach($arr AS $color){
-    print $color . "<br>";
-
-}
-
-print dirname(__FILE__);
-print "<br>";
-print "<br>";
-print $_SERVER["PHP_SELF"];
-print "<br>";
-print "<br>";
-print basename($_SERVER["PHP_SELF"]);
-print "<br>";
-print "<br>";
-
-if(file_exists("index.php")AND is_readable("index.php")){
-    print "yes";
-}else {
-        print "no";
-    }*/
-
-
-    
-?>
+// print 
+// print "<br>";
+// print "<br>";
+// print $_SERVER["PHP_SELF"];
+// print "<br>";
+// print "<br>";
+// print basename($_SERVER["PHP_SELF"]);
+// print "<br>";
+// print "<br>";
+// if(file_exists("index.php") AND is_readable("index.php")){
+//     print "yes";
+// }else{
+//     print "no";
+// }
